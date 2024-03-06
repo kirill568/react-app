6243bfd4a5ecb864d827d9fca9b23f1d992bc9b4
kirill568 <kirill568@gmail.com>
2024-03-06 21:40:13 +0700

    lab 4: redux

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import ErrorPage from './templates/ErrorPage.jsx'
 import Lab1 from './templates/labs/Lab1'
 import Lab2 from './templates/labs/Lab2'
 import Lab4 from './templates/labs/Lab4'
+import store from './store.js'
+import { Provider } from 'react-redux'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -35,6 +37,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
--- src/templates/labs/Lab4/Lab4.jsx
+++ src/templates/labs/Lab4/Lab4.jsx
@@ -1,15 +1,20 @@
 import Button from "../../../components/Button"
 import ThemeContext from "../../../contexts/ThemeContext"
 import { THEME_LIGHT, THEME_DARK } from "../../../contexts/ThemeContext"
-import { BUTTON_COLOR_ORANGE } from "../../../components/Button/config"
+import { BUTTON_COLOR_ORANGE, BUTTON_COLOR_RED, BUTTON_COLOR_GREEN } from "../../../components/Button/config"
 import { useContext, useState, useEffect } from "react"
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, setValue, selectCount } from "../../../features/counter/counterSlice"
 
 const Lab4 = () => {
     const { theme, setTheme } = useContext(ThemeContext)
     const [count, setCount] = useState(0)
+    const countRedux = useSelector(selectCount)
+    const dispatch = useDispatch()
 
     useEffect(() => {
         setCount(0)
+        dispatch(setValue(0))
     }, [theme])
 
     return (
@@ -20,7 +25,7 @@ const Lab4 = () => {
                 ></Button>
 
                 <Button
-                    label="dark theme"
+                    label="Dark theme"
                     onClick={() => setTheme(THEME_DARK)}
                 ></Button>
 
@@ -30,6 +35,21 @@ const Lab4 = () => {
                         color={ BUTTON_COLOR_ORANGE }
                         label="Click me"
                         onClick={() => setCount(count + 1)}
+                    ></Button>
+                </div>
+
+                <div className="lab4-template__clicker-wrapper">
+                    <span>You clicked {countRedux} times (Redux)</span>
+                    <Button
+                        color={ BUTTON_COLOR_GREEN }
+                        label="Increment"
+                        onClick={() => dispatch(increment())}
+                    ></Button>
+
+                    <Button
+                        color={ BUTTON_COLOR_RED }
+                        label="Decrement "
+                        onClick={() => dispatch(decrement())}
                     ></Button>
                 </div>
             </div>
